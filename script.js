8aab59640ca931b4afe81cb0377839ecfc44e865
Cratior <55932656+Cratior@users.noreply.github.com>
2023-03-21 23:15:02 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-// Toggle menu on button click
-const menuToggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('show');
-});
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+  
+  function scrollToElement(id) {
+    var element = document.querySelector(id);
+    element.scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
